3fa0803facf2a351ff34635cdc05a1b9f89f5fc4
rcpauor32 <rcolivepau@gmail.com>
2018-04-15 15:04:18 +0200

    BaseClasses for AI Nemesis

---
+++ Source/ProjectNemesis/NemesisAIController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "NemesisAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTNEMESIS_API ANemesisAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/ProjectNemesis/NemesisPatrolPoint.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TargetPoint.h"
+#include "NemesisPatrolPoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTNEMESIS_API ANemesisPatrolPoint : public ATargetPoint
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
